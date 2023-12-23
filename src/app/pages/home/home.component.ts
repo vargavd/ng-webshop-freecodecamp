f1d0ad9530ed835b9142439197f604f73edb92e8
Viktor Daniel Varga <vvd007@gmail.com>
2023-12-23 04:46:16 +0100

    Homepage UI

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+const ROWS_HEIGHT: { [id:number]:number} = { 1: 400, 3: 335, 4: 350}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
   cols = 3;
+  category: string | undefined;
+  rowHeight = ROWS_HEIGHT[this.cols];
 
   constructor() { }
 
@@ -14,5 +18,10 @@ export class HomeComponent implements OnInit {
 
   onColumnsCountChange(colsNum: number): void {
     this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
+  }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
   }
 }
