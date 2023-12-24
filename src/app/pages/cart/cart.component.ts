3b4e0ee392a8811cbf117c5d00d84d055879aa39
Viktor Daniel Varga <vvd007@gmail.com>
2023-12-24 03:33:02 +0100

    Cart UI finished

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -6,27 +6,40 @@ import { Cart, CartItem } from 'src/app/models/cart.model';
   templateUrl: './cart.component.html'
 })
 export class CartComponent implements OnInit {
-  cart: Cart = { items: [{
-    product: 'https://via.placeholder.com/150',
-    name: 'Snickers',
-    price: 150,
-    quantity: 1,
-    id: 1
-  }]};
+  cart: Cart = { items: [
+    {
+      product: 'https://via.placeholder.com/150',
+      name: 'Snickers',
+      price: 150,
+      quantity: 1,
+      id: 1
+    },
+    {
+      product: 'https://via.placeholder.com/150',
+      name: 'Snickers',
+      price: 150,
+      quantity: 3,
+      id: 2
+    },
+  ]};
   dataSource: Array<CartItem> = [];
   displayedColumns: Array<string> = [
     'product',
-    // 'name',
-    // 'price',
-    // 'quantity',
-    // 'total',
-    // 'action'
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action'
   ]
 
   constructor() { }
 
   ngOnInit(): void {
     this.dataSource = this.cart.items;
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return items.map((item) => item.price * item.quantity).reduce((prev, current) => prev + current, 0);
   }
 
 }
